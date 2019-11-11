94248c5557e9ac415ee4407e466f7f040eb72398
geracimov <geracimov@gmail.com>
2019-11-11 23:31:12 +0300

    up parent version to 1.0.1.  Added eureka client & cloud config client.

--- lh-raspberryserver/src/main/java/ru/geracimov/otus/spring/lighthouse/componentserver/LighthouseApplication.java
+++ lh-raspberryserver/src/main/java/ru/geracimov/otus/spring/lighthouse/componentserver/LighthouseApplication.java
@@ -2,8 +2,10 @@ package ru.geracimov.otus.spring.lighthouse.componentserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@EnableEurekaClient
 @EnableAspectJAutoProxy
 @SpringBootApplication
 public class LighthouseApplication {
